238572830d4dab139b03059bda562d3dfd6682ad
Eren Emre Korkmaz <erenkorkmazemre@gmail.com>
2023-07-03 17:43:33 +0200

    feature: common actions added.

--- pages/CommonPage.ts
+++ pages/CommonPage.ts
@@ -7,7 +7,19 @@ export async function click(stepDescription: string, page: Page, selector: strin
     console.log(stepDescription);
 }
 
+//Fill the input with value (Clear the input then fill)
 export async function fillTheInput(
+    stepDescription: string,
+    page: Page,
+    locator,
+    value: any
+): Promise<void> {
+    await page.locator(locator).fill(value);
+    console.log(stepDescription);
+}
+
+//Type the input with value
+export async function typeTheInput(
     stepDescription: string,
     page: Page,
     locator,
@@ -30,6 +42,61 @@ export async function mockDataWithMultipleArray(
             body: JSON.stringify(singleton)
         });
     });
+    console.log(stepDescription);
+}
+
+//Click the element
+export async function clickTheElement(
+    stepDescription: string,
+    page: Page,
+    locator,
+    value: any
+): Promise<void> {
+    await page.locator(locator).click();
+    console.log(stepDescription);
+}
+
+//Force Click the element
+export async function forceClickTheElement(
+    stepDescription: string,
+    page: Page,
+    locator,
+    value: any
+): Promise<void> {
+    await page.locator(locator).click();
+    console.log(stepDescription);
+}
+
+//Double Click the element
+export async function doubleClickTheElement(
+    stepDescription: string,
+    page: Page,
+    locator,
+    value: any
+): Promise<void> {
+    await page.locator(locator).click();
+    console.log(stepDescription);
+}
+
+//Submits on element with given element
+export async function submitTheElementWithGivenElement(
+    stepDescription: string,
+    page: Page,
+    locator,
+    value: any
+): Promise<void> {
+    await page.locator(locator).click();
+    console.log(stepDescription);
+}
+
+//Find element by given Text
+export async function findElementByGivenText(
+    stepDescription: string,
+    page: Page,
+    locator,
+    value: any
+): Promise<void> {
+    await page.locator(locator).check();
     console.log(stepDescription);
 }
 
